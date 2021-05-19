40d5866b5a3851a5979a5a25259f272eeea2ec57
le5tef <66320826+le5tef@users.noreply.github.com>
2021-05-19 22:32:01 +0300

    fixed

---
+++ script.js
@@ -0,0 +1,38 @@
+var pos = 1;
+function counter(a) {
+    prevPos = pos;
+    pos = a;
+    if (pos > 5) { pos = 5 };
+    if (pos < 1) { pos = 1 };
+    const shortText = document.querySelectorAll('.progress__shortText')
+    const barControl = document.querySelectorAll('.progress__bar_start');
+    const barTransControl = document.querySelector('.progress__bar_start')
+    const btns = document.querySelectorAll('.progress__icon');
+    const blocks = document.querySelectorAll('.progress__price');
+    for (let k = 0; k < btns.length; k += 1) {
+        btns[k].style.backgroundColor = "aliceblue";
+        btns[k].style.color = "#37b2ed";
+        blocks[k].style.display = "none";
+        shortText[k].style.color = "#11557a";
+    }
+    var latency = 750,
+        k = prevPos,
+        numIterations = pos;
+    var loop = function () {
+        if (k < numIterations) {
+            barControl[k - 1].style.width = "100%";
+            k += 1;
+            setTimeout(loop, latency);
+        }
+        if (k > numIterations) {
+            barControl[k - 2].style.width = "0%";
+            k -= 1;
+            setTimeout(loop, latency);
+        }
+    }
+    loop()
+    shortText[pos - 1].style.color = "aliceblue";
+    btns[pos - 1].style.backgroundColor = "#37b2ed";
+    btns[pos - 1].style.color = "aliceblue";
+    blocks[pos - 1].style.display = "flex";
+};
