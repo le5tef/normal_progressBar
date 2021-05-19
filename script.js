a9f1de895f5c3a01f913595c027514769fa88280
le5tef <66320826+le5tef@users.noreply.github.com>
2021-05-20 00:27:39 +0300

    finally fixed

--- script.js
+++ script.js
@@ -1,12 +1,14 @@
 var pos = 1;
+var isRunning = 0;
 function counter(a) {
+    if (isRunning) { return }
+    isRunning = 1;
     prevPos = pos;
     pos = a;
     if (pos > 5) { pos = 5 };
     if (pos < 1) { pos = 1 };
-    const shortText = document.querySelectorAll('.progress__shortText')
+    const shortText = document.querySelectorAll('.progress__shortText');
     const barControl = document.querySelectorAll('.progress__bar_start');
-    const barTransControl = document.querySelector('.progress__bar_start')
     const btns = document.querySelectorAll('.progress__icon');
     const blocks = document.querySelectorAll('.progress__price');
     for (let k = 0; k < btns.length; k += 1) {
@@ -15,19 +17,26 @@ function counter(a) {
         blocks[k].style.display = "none";
         shortText[k].style.color = "#11557a";
     }
-    var latency = 750,
+    var latency = 500,
         k = prevPos,
         numIterations = pos;
+
     var loop = function () {
+
         if (k < numIterations) {
             barControl[k - 1].style.width = "100%";
             k += 1;
+
             setTimeout(loop, latency);
         }
-        if (k > numIterations) {
+        else if (k > numIterations) {
             barControl[k - 2].style.width = "0%";
             k -= 1;
+
             setTimeout(loop, latency);
+        }
+        else {
+            isRunning = 0;
         }
     }
     loop()
